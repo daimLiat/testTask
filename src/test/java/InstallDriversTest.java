5a20a5d37d730ce0d66ba5a98ba16b1905e30773
Dima Tsygankov <daim.tsyg@gmail.com>
2022-07-11 01:16:34 +0300

    initial commit

---
+++ src/test/java/InstallDriversTest.java
@@ -0,0 +1,28 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+
+public class InstallDriversTest {
+
+    @Test
+    public void chromeSession() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.selenium.dev/");
+    }
+
+    @Test
+    public void firefoxSession() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.selenium.dev/");
+    }
+}

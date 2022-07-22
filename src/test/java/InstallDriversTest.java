db14d76d162531a8673a4c4bd3c778620bfac73c
Dima Tsygankov <daim.tsyg@gmail.com>
2022-07-23 01:01:55 +0300

    added one more test class and enabled driver-tests

--- src/test/java/InstallDriversTest.java
+++ src/test/java/InstallDriversTest.java
@@ -1,29 +1,26 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.Ignore;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
-@Disabled
 public class InstallDriversTest {
 
     @Test
     public void chromeSession() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.selenium.dev/");
+        driver.get("https://www.jetbrains.com/teamcity/");
     }
 
     @Test
     public void firefoxSession() {
         WebDriverManager.firefoxdriver().setup();
         WebDriver driver = new FirefoxDriver();
-        driver.get("https://www.selenium.dev/");
+        driver.get("https://www.jetbrains.com/teamcity/");
     }
 }

a2d533013ae9e8ea941e6e5919af29c31c2210a6
Dima Tsygankov <daim.tsyg@gmail.com>
2022-07-31 00:49:10 +0300

    revert deleting of driver-tests

---
+++ src/test/java/com/github/daimLiat/InstallDriverTest.java
@@ -0,0 +1,28 @@
+package com.github.daimLiat;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class InstallDriverTest {
+
+    @Test
+    public void chromeSession() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.jetbrains.com/teamcity/");
+    }
+
+    @Test
+    public void firefoxSession() {
+        WebDriverManager.firefoxdriver().setup();
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.jetbrains.com/teamcity/");
+    }
+}
